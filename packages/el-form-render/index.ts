a6da82b81feb6060579a31773018731eb448454b
lzh <null>
2023-08-03 21:08:21 +0800

    ...

--- packages/el-form-render/index.ts
+++ packages/el-form-render/index.ts
@@ -1,12 +1,19 @@
+// @ts-nocheck
+
 import type { App, Plugin } from 'vue'
 import FormRender from './form-render.vue'
+export * from './form-render.ts'
+
+import { ElForm } from 'element-plus'
+import 'element-plus/es/components/form/style/css'
 
 type SFCWithInstall<T> = T & Plugin
 
 function withInstall<T>(comp: T): SFCWithInstall<T> {
-  // @ts-ignore
-  comp.install = (app: App) => app.component(comp.name, comp)
-  // @ts-ignore
+  comp.install = (app: App) => {
+    app.component(comp.name, comp)
+    app.use(ElForm)
+  }
   return comp
 }
 
