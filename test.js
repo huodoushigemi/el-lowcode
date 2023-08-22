fecb0764af203fbbad99bad76f68c03fcf6d07ca
2629878269 <2629878269@qq.com>
2023-08-23 01:47:52 +0800

    ...

---
+++ test.js
@@ -0,0 +1,18 @@
+import { renameSync, rmdirSync } from 'fs'
+import { join } from 'path'
+import { removeSync } from 'fs-extra/esm'
+import { execSync } from 'child_process'
+
+
+
+// console.info('==== pnpm docs:build')
+// execSync('pnpm docs:build')
+
+console.info('==== remove dest')
+removeSync('docs/.vitepress/dest')
+renameSync('docs/.vitepress/dist', 'docs/.vitepress/dest')
+
+console.info('==== git **')
+execSync('git add */dest/*')
+execSync('git commit -m docs')
+// execSync('git push')
