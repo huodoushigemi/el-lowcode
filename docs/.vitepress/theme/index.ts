59d7a354e0c7152965f9dc0193f85a44d4b1cb37
lzh <null>
2023-08-23 19:22:53 +0800

    fix(docs): fix bug on ssr

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,7 +1,10 @@
+import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
 import { EnhanceAppContext } from 'vitepress'
+import CodePreview from 'vite-plugin-markdown-preview/component/CodePreview.vue'
 
-import ElementPlus from 'element-plus'
+import { useMounted } from '@vueuse/core'
+import ElementPlus, { ID_INJECTION_KEY } from 'element-plus'
 import 'element-plus/dist/index.css'
 import 'element-plus/theme-chalk/dark/css-vars.css'
 // import 'virtual:uno.css'
@@ -17,5 +20,14 @@ export default {
   enhanceApp(ctx: EnhanceAppContext) {
     // DefaultTheme.enhanceApp(ctx)
     ctx.app.use(ElementPlus)
-  },
+    ctx.app.provide(ID_INJECTION_KEY, { prefix: 1024, current: 0 })
+
+    // Fix bug on SSR: https://element-plus.gitee.io/zh-CN/guide/ssr.html#%E5%9C%A8%E6%8C%82%E8%BD%BD%E6%97%B6%E6%B8%B2%E6%9F%93-teleport
+    ctx.app.component('CodePreview', {
+      setup: (props, { slots }) => {
+        const isMounted = useMounted()
+        return () => isMounted.value ? h(CodePreview, props, slots) : undefined
+      }
+    })
+  }, 
 }
