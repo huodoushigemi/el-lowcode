c1d54cbb967f922172522b1af208dc50a667aacf
2629878269 <2629878269@qq.com>
2023-10-13 21:51:39 +0800

    fix(designer): preview template

--- designer/pages/index.vue
+++ designer/pages/index.vue
@@ -1,16 +1,23 @@
 <script setup lang="ts">
 import { computed, ref } from 'vue'
+import { useRouter } from 'vue-router'
 import Designer from '@el-lowcode/designer'
 import { computedAsync } from '@vueuse/core'
 
-const designer = ref()
-const schema = computed(() => encodeURIComponent(JSON.stringify(designer.value.root)))
+const router = useRouter()
 
-const ___ = Object.values(import.meta.glob('../template/*.ts'))
-const templates = computedAsync(async () => ((await Promise.all(___.map(e => e()))).map(e => e.default).filter(e => e)))
+const designer = ref()
+const encodeSchema = (schema) => encodeURIComponent(JSON.stringify(schema))
+const schema = computed(() => encodeSchema(designer.value.root))
+
+const templateModules = Object.values(import.meta.glob('../template/*.ts'))
+const templates = computedAsync(async () => ((await Promise.all(templateModules.map(e => e()))).map(e => e.default).filter(e => e)))
 
 function onEdit(item) {
   designer.value.root = item.schema()
+}
+function demoUrl(schema) {
+  return router.resolve({ path: '/demo', query: { schema: encodeSchema(schema) } })
 }
 </script>
 
@@ -19,7 +26,7 @@ function onEdit(item) {
     <Designer ref="designer" h100vh>
       <!-- 额外按钮 -->
       <template #actions>
-        <el-tooltip content="preview"><i-mdi:play-circle-outline bg-hover @click="$router.push({ path: 'demo', query: { schema } })" /></el-tooltip>
+        <el-tooltip content="preview"><i-mdi:play-circle-outline bg-hover @click="$router.push(demoUrl(designer.root).fullPath)" /></el-tooltip>
         <a href="https://github.com/huodoushigemi/el-lowcode" target="_blank" c="unset" bg-hover><i-bytesize:github wfull hfull /></a>
       </template>
       
@@ -34,8 +41,8 @@ function onEdit(item) {
               <div py8 px12>
                 <div font-bold>{{ item.title }}</div>
                 <div text-right>
-                  <el-button size="default" type="primary" plain text bg tag="a" href="" target="_blank" p8 decoration-none><i-mdi:eye-outline text-1.2em /></el-button>
-                  <el-button size="default" type="primary" plain text bg p8 @click="onEdit(item)"><i-mdi:application-import text-1.2em /></el-button>
+                  <el-button size="default" type="primary" title="预览" plain text bg tag="a" :href="demoUrl(item.schema()).href" target="_blank" p8 decoration-none><i-mdi:eye-outline text-1.2em /></el-button>
+                  <el-button size="default" type="primary" title="导入" plain text bg p8 @click="onEdit(item)"><i-mdi:application-import text-1.2em /></el-button>
                 </div>
               </div>
             </el-card>
