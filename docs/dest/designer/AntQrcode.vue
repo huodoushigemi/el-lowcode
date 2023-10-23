b3517d5f47f1357bf233e765f6b02d44058ace25
2629878269 <2629878269@qq.com>
2023-10-23 23:35:18 +0800

    docs

---
+++ docs/dest/designer/AntQrcode.vue
@@ -0,0 +1,28 @@
+<script>
+import { QRCode } from 'https://cdn.jsdelivr.net/npm/ant-design-vue@4.0.6/dist/antd.esm.min.js'
+
+export default QRCode
+
+export const el_lowcode = {
+  // 组件显示名
+  label: '二维码',
+  // 右侧的属性面板
+  props: [
+    { lp: ['url', 'value'] },
+    { lp: 'icon' },
+    { lp: 'type', type: 'radio-group', displayValue: 'canvas', options: ['canvas', 'svg'] },
+    { lp: 'bg-color', type: 'color-picker' },
+    { lp: 'error-level', type: 'radio-group', displayValue: 'M', options: ['L', 'M', 'Q', 'H'] },
+  ],
+  // 属性默认值
+  defaultProps: () => ({
+    value: 'https://www.antdv.com/',
+    icon: 'https://www.antdv.com/assets/logo.1ef800a8.svg',
+    bgColor: '#ffffff80'
+  })
+}
+</script>
+
+<style>
+@import 'https://cdn.jsdelivr.net/npm/ant-design-vue@4.0.6/dist/reset.css'
+</style>
---
+++ docs/dest/designer/SlSplitPanel.vue
@@ -0,0 +1,28 @@
+<script>
+import 'https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.11.0/cdn/components/split-panel/split-panel.js'
+import uuid from 'https://cdn.jsdelivr.net/npm/uuid/dist/esm-browser/v4.js'
+
+export const el_lowcode = {
+  is: 'sl-split-panel',
+  // 组件显示名
+  label: '分割面板',
+  layout: true,
+  // 右侧的属性面板
+  props: [
+    { lp: 'position', type: 'slider' },
+    { lp: 'vertical', type: 'switch', displayValue: false },
+    { lp: 'disabled', type: 'switch', displayValue: false },
+  ],
+  // 属性默认值
+  defaultProps: () => ({
+    children: [
+      { is: 'div', _id: uuid(), slot: 'start', children: [] },
+      { is: 'div', _id: uuid(), slot: 'end', children: [] },
+    ]
+  })
+}
+</script>
+
+<style>
+@import 'https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.11.0/cdn/themes/dark.css';
+</style>
