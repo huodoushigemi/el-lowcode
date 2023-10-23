151508b52f3f83aba5d5b0ec58f83a7771dd47da
2629878269 <2629878269@qq.com>
2023-10-23 21:53:57 +0800

    docs: file

---
+++ public/MyAudio.vue
@@ -0,0 +1,27 @@
+<template>
+  <div style="padding: 20px">
+    <p>我是自定义的 audio 组件</p>
+    <audio v-bind="$attrs" />
+  </div>
+</template>
+
+<script setup>
+defineOptions({ name: 'MyAudio' })
+</script>
+
+<script>
+export const el_lowcode = {
+  is: 'MyAudio',
+  label: '音频',
+  props: [
+    { lp: 'src' },
+    { lp: 'autoplay', type: 'switch' },
+    { lp: 'loop', type: 'switch' },
+    { lp: 'muted', type: 'switch' },
+  ],
+  defaultProps: () => ({
+    controls: true,
+    src: 'https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3',
+  })
+}
+</script>
---
+++ public/MyCard.vue
@@ -0,0 +1,75 @@
+<template>
+  <!-- see https://shoelace.style/components/card -->
+  <sl-card class="card-overview" v-bind="$attrs">
+    <img slot="image" :src="cover" />
+
+    <strong>{{ title }}</strong><br />
+
+    {{ desc }}<br />
+
+    <small>6 weeks old</small>
+
+    <div slot="footer">
+      <sl-button variant="primary" pill @click="onClick">More Info</sl-button>
+      <sl-rating></sl-rating>
+    </div>
+  </sl-card>
+</template>
+
+<script setup>
+import 'https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.11.0/cdn/components/card/card.js'
+import 'https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.11.0/cdn/components/button/button.js'
+import 'https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.11.0/cdn/components/rating/rating.js'
+
+// 组件名（必须）
+defineOptions({ name: 'MyCard' })
+
+defineProps({
+  cover: String,
+  title: String,
+  desc: String,
+})
+
+function onClick() {
+  alert('clicked')
+}
+</script>
+
+<script>
+export const el_lowcode = {
+  // 组件显示名
+  label: '卡片',
+  // 右侧的属性面板
+  props: [
+    { lp: 'cover' },
+    { lp: 'title' },
+    { lp: 'desc' },
+  ],
+  // 属性默认值
+  defaultProps: () => ({
+    cover: 'https://images.unsplash.com/photo-1559209172-0ff8f6d49ff7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80',
+    title: 'Mittens',
+    desc: 'This kitten is as cute as he is playful. Bring him home today'
+  })
+}
+</script>
+
+<style>
+@import 'https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.11.0/cdn/themes/dark.css';
+
+.card-overview {
+  max-width: 300px;
+  font-size: 16px;
+  line-height: 1.8;
+}
+
+.card-overview small {
+  color: var(--sl-color-neutral-500);
+}
+
+.card-overview [slot='footer'] {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
