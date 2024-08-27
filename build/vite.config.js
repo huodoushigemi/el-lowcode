8703a4ad498189b2f915b6cdbd2be146d2ca3909
2629878269 <2629878269@qq.com>
2024-08-27 04:10:52 +0000

    update build/vite.config.js.
    Signed-off-by: 2629878269 <2629878269@qq.com>

--- build/vite.config.js
+++ build/vite.config.js
@@ -1,46 +1,13 @@
-// import { build } from './build.js'
 import path from 'path'
-import { build, defineConfig } from 'vite'
+import { build, defineConfig, mergeConfig } from 'vite'
 import fs from 'fs'
 import fse from 'fs-extra/esm'
-import { cwd } from './utils.js'
-import { entries } from './plugins/alias.js'
 import { ALL_DEPS, ALL_PKGS } from './all-pkgs.js'
-
-// @ts-ignore
-// export default defineConfig(async () => ({
-
-export const defaultConfig = defineConfig({
-  root: cwd,
-  resolve: {
-    alias: [
-      ...entries,
-    ]
-  },
-  plugins: [
-    (await import('unplugin-vue-macros/vite')).default({
-      plugins: {
-        vue: (await import('@vitejs/plugin-vue')).default(),
-        vueJsx: (await import('@vitejs/plugin-vue-jsx')).default(), // if needed
-      }
-    }),
-    (await import('unocss/vite')).default(),
-    (await import('unplugin-vue-components/vite')).default({
-      resolvers: [(await import('unplugin-icons/resolver')).default()]
-    }),
-    (await import('unplugin-icons/vite')).default({ autoInstall: true }),
-  ]
-})
+import { defaultConfig } from './defaultConfig.js'
 
 async function build1(input, outDir) {
-  await build({
+  await build(mergeConfig(defaultConfig, defineConfig({
     configFile: false,
-    root: cwd,
-    resolve: {
-      alias: [
-        ...entries,
-      ]
-    },
     build: {
       outDir,
       target: 'esnext',
@@ -49,7 +16,6 @@ async function build1(input, outDir) {
         formats: ['es'],
         fileName: '[name]',
       },
-      cssCodeSplit: true,
       minify: false,
       copyPublicDir: false,
       rollupOptions: {
@@ -61,28 +27,10 @@ async function build1(input, outDir) {
       }
     },
     plugins: [
-      (await import('unplugin-vue-macros/vite')).default({
-        plugins: {
-          vue: (await import('@vitejs/plugin-vue')).default(),
-          vueJsx: (await import('@vitejs/plugin-vue-jsx')).default(), // if needed
-        }
-      }),
       (await import('vite-plugin-css-injected-by-js')).default({ relativeCSSInjection: true }),
-      (await import('unocss/vite')).default(),
-      (await import('unplugin-vue-components/vite')).default({
-        resolvers: [(await import('unplugin-icons/resolver')).default()]
-      }),
-      (await import('unplugin-icons/vite')).default({ autoInstall: true }),
-
-      (await import('rollup-plugin-external-globals')).default(id => (
-        id == 'vue' ? 'Vue' :
-        id == 'vue-demi' ? 'Vue' :
-        id.includes('monaco-editor') ? 'MonacoEditor' : void 0
-      ))
-
       // (await import('rollup-plugin-visualizer')).visualizer(),
     ]
-  })
+  })))
 
   ;['package.json', 'README.md'].forEach(name => {
     const file = path.join(path.dirname(input), name)
