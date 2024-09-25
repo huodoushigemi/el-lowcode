f072bdaaa00b8ca628576690216298111309b312
2629878269 <2629878269@qq.com>
2024-09-26 03:21:06 +0800

    commit

--- plugins/template/.lowcode/TemplateView.vue
+++ plugins/template/.lowcode/TemplateView.vue
@@ -30,14 +30,36 @@ function onEdit(item) {
   designerCtx.root = JSON.parse(JSON.stringify(item.schema))
 }
 
-// return router.resolve({ path: '/demo', query: { schema: encodeSchema(schema) } })
-function demoUrl({ title, schema }) {
+async function demoUrl({ title, schema }) {
   const sw = screen.width, sh = screen.height
   const w = Math.min(parseInt(schema.designer?.canvas?.style?.width || sw), sw)
   const h = Math.min(parseInt(schema.designer?.canvas?.style?.height || sh), sh)
 
+  const text = JSON.stringify(schema)
+  const hash = (await import('md5')).default(text)
+
+  const { createClient } = await import('https://esm.sh/@supabase/supabase-js@2.45.4')
+  const supabase = createClient(
+    'https://oxbkrsyagojtbckytbjx.supabase.co',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im94Ymtyc3lhZ29qdGJja3l0Ymp4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjcyNzQzNTAsImV4cCI6MjA0Mjg1MDM1MH0.M3O3VPvgfaxtMaasBIM-rgPw90l1GAG_m-_RQqMbNMc'
+  )
+
+  // await supabase.auth.signUp({ email: '123@qq.com', password: '123456' })
+
+  const storage = supabase.storage.from('lcd')
+
+  try {
+    const file = new File([text], hash, { type: 'text/plain' })
+    await storage.upload(hash, file, { cacheControl: 3600 * 24 * 7 })
+  } catch (e) {
+    console.error(e)
+  }
+
+  // eg: https://oxbkrsyagojtbckytbjx.supabase.co/storage/v1/object/public/lcd/echarts-demo.js
+  const url = `${storage.url}/object/public/${storage.bucketId}/${hash}`
+
   const win = window.open(
-    `/demo.html?schema=${encodeSchema(schema)}`,
+    `/demo.html?file=${encodeURIComponent(url)}`,
     '_blank',
     `popup,width=${w},height=${h},left=${sw - w >> 1},top=${sh - h >> 1}`
   )
@@ -47,5 +69,11 @@ function demoUrl({ title, schema }) {
   })
 
   return {}
+}
+
+async function checkFileExists(storage, path) {
+  const { data, error } = await storage.getPublicUrl(path)
+  if (error?.message?.includes('not found')) return
+  return data.publicUrl
 }
 </script>
