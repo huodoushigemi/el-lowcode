ae78bf017c6d596f08ba08e389fbb4e35f7fad45
huodoushigemi <2629878269@qq.com>
2024-10-30 10:33:47 +0000

    commit

--- plugins/antv/.lowcode/index.js
+++ plugins/antv/.lowcode/index.js
@@ -10,16 +10,19 @@ export const widgets = [
       const ks = Object.keys(node?.$data.dataCfg.data?.[0] || {})
       
       return [
+        { lp: ['数据', 'dataCfg.data'], script: true },
         { lp: ['行维度', 'dataCfg.fields.rows'], options: ks, el: { multiple: true } },
         { lp: ['列维度', 'dataCfg.fields.columns'], options: ks, el: { multiple: true } },
         { lp: ['指标', 'dataCfg.fields.values'], options: ks, set: v => toArr(v), get: v => v?.[0] },
-        { lp: ['数据', 'dataCfg.data'], script: true },
+        { lp: ['主题', 'themeCfg.name'], options: [['—', void 0], 'colorful', 'gray', 'dark'], el: { class: 'wfull' } },
+        { lp: ['', 'brandColor'], type: 'color-picker', el: { colorFormat: 'hex' } }
       ]
     },
     defaultProps: () => ({
       style: { height: '338px' },
       options: {},
       dataCfg: {
+        data: `{{[\n  { "number": 7789, "province": "浙江省", "city": "杭州市", "type": "家具", "sub_type": "桌子" },\n  { "number": 2367, "province": "浙江省", "city": "绍兴市", "type": "家具", "sub_type": "桌子" },\n  { "number": 3877, "province": "浙江省", "city": "宁波市", "type": "家具", "sub_type": "桌子" },\n  { "number": 4342, "province": "浙江省", "city": "舟山市", "type": "家具", "sub_type": "桌子" },\n  { "number": 5343, "province": "浙江省", "city": "杭州市", "type": "家具", "sub_type": "沙发" },\n  { "number": 632, "province": "浙江省", "city": "绍兴市", "type": "家具", "sub_type": "沙发" },\n  { "number": 7234, "province": "浙江省", "city": "宁波市", "type": "家具", "sub_type": "沙发" },\n  { "number": 834, "province": "浙江省", "city": "舟山市", "type": "家具", "sub_type": "沙发" },\n  { "number": 945, "province": "浙江省", "city": "杭州市", "type": "办公用品", "sub_type": "笔" },\n  { "number": 1304, "province": "浙江省", "city": "绍兴市", "type": "办公用品", "sub_type": "笔" },\n  { "number": 1145, "province": "浙江省", "city": "宁波市", "type": "办公用品", "sub_type": "笔" },\n  { "number": 1432, "province": "浙江省", "city": "舟山市", "type": "办公用品", "sub_type": "笔" },\n  { "number": 1343, "province": "浙江省", "city": "杭州市", "type": "办公用品", "sub_type": "纸张" },\n  { "number": 1354, "province": "浙江省", "city": "绍兴市", "type": "办公用品", "sub_type": "纸张" },\n  { "number": 1523, "province": "浙江省", "city": "宁波市", "type": "办公用品", "sub_type": "纸张" },\n  { "number": 1634, "province": "浙江省", "city": "舟山市", "type": "办公用品", "sub_type": "纸张" },\n  { "number": 1723, "province": "四川省", "city": "成都市", "type": "家具", "sub_type": "桌子" },\n  { "number": 1822, "province": "四川省", "city": "绵阳市", "type": "家具", "sub_type": "桌子" },\n  { "number": 1943, "province": "四川省", "city": "南充市", "type": "家具", "sub_type": "桌子" },\n  { "number": 2330, "province": "四川省", "city": "乐山市", "type": "家具", "sub_type": "桌子" },\n  { "number": 2451, "province": "四川省", "city": "成都市", "type": "家具", "sub_type": "沙发" },\n  { "number": 2244, "province": "四川省", "city": "绵阳市", "type": "家具", "sub_type": "沙发" },\n  { "number": 2333, "province": "四川省", "city": "南充市", "type": "家具", "sub_type": "沙发" },\n  { "number": 2445, "province": "四川省", "city": "乐山市", "type": "家具", "sub_type": "沙发" },\n  { "number": 2335, "province": "四川省", "city": "成都市", "type": "办公用品", "sub_type": "笔" },\n  { "number": 245, "province": "四川省", "city": "绵阳市", "type": "办公用品", "sub_type": "笔" },\n  { "number": 2457, "province": "四川省", "city": "南充市", "type": "办公用品", "sub_type": "笔" },\n  { "number": 2458, "province": "四川省", "city": "乐山市", "type": "办公用品", "sub_type": "笔" },\n  { "number": 4004, "province": "四川省", "city": "成都市", "type": "办公用品", "sub_type": "纸张" },\n  { "number": 3077, "province": "四川省", "city": "绵阳市", "type": "办公用品", "sub_type": "纸张" },\n  { "number": 3551, "province": "四川省", "city": "南充市", "type": "办公用品", "sub_type": "纸张" },\n  { "number": 352, "province": "四川省", "city": "乐山市", "type": "办公用品", "sub_type": "纸张" }\n]\n}}`,
         fields: {
           rows: ['province', 'city'],
           columns: ['type', 'sub_type'],
@@ -33,42 +36,8 @@ export const widgets = [
           { field: 'type', name: '类别' },
           { field: 'sub_type', name: '子类别' },
         ],
-        data: `{{[
-  { "number": 7789, "province": "浙江省", "city": "杭州市", "type": "家具", "sub_type": "桌子" },
-  { "number": 2367, "province": "浙江省", "city": "绍兴市", "type": "家具", "sub_type": "桌子" },
-  { "number": 3877, "province": "浙江省", "city": "宁波市", "type": "家具", "sub_type": "桌子" },
-  { "number": 4342, "province": "浙江省", "city": "舟山市", "type": "家具", "sub_type": "桌子" },
-  { "number": 5343, "province": "浙江省", "city": "杭州市", "type": "家具", "sub_type": "沙发" },
-  { "number": 632, "province": "浙江省", "city": "绍兴市", "type": "家具", "sub_type": "沙发" },
-  { "number": 7234, "province": "浙江省", "city": "宁波市", "type": "家具", "sub_type": "沙发" },
-  { "number": 834, "province": "浙江省", "city": "舟山市", "type": "家具", "sub_type": "沙发" },
-  { "number": 945, "province": "浙江省", "city": "杭州市", "type": "办公用品", "sub_type": "笔" },
-  { "number": 1304, "province": "浙江省", "city": "绍兴市", "type": "办公用品", "sub_type": "笔" },
-  { "number": 1145, "province": "浙江省", "city": "宁波市", "type": "办公用品", "sub_type": "笔" },
-  { "number": 1432, "province": "浙江省", "city": "舟山市", "type": "办公用品", "sub_type": "笔" },
-  { "number": 1343, "province": "浙江省", "city": "杭州市", "type": "办公用品", "sub_type": "纸张" },
-  { "number": 1354, "province": "浙江省", "city": "绍兴市", "type": "办公用品", "sub_type": "纸张" },
-  { "number": 1523, "province": "浙江省", "city": "宁波市", "type": "办公用品", "sub_type": "纸张" },
-  { "number": 1634, "province": "浙江省", "city": "舟山市", "type": "办公用品", "sub_type": "纸张" },
-  { "number": 1723, "province": "四川省", "city": "成都市", "type": "家具", "sub_type": "桌子" },
-  { "number": 1822, "province": "四川省", "city": "绵阳市", "type": "家具", "sub_type": "桌子" },
-  { "number": 1943, "province": "四川省", "city": "南充市", "type": "家具", "sub_type": "桌子" },
-  { "number": 2330, "province": "四川省", "city": "乐山市", "type": "家具", "sub_type": "桌子" },
-  { "number": 2451, "province": "四川省", "city": "成都市", "type": "家具", "sub_type": "沙发" },
-  { "number": 2244, "province": "四川省", "city": "绵阳市", "type": "家具", "sub_type": "沙发" },
-  { "number": 2333, "province": "四川省", "city": "南充市", "type": "家具", "sub_type": "沙发" },
-  { "number": 2445, "province": "四川省", "city": "乐山市", "type": "家具", "sub_type": "沙发" },
-  { "number": 2335, "province": "四川省", "city": "成都市", "type": "办公用品", "sub_type": "笔" },
-  { "number": 245, "province": "四川省", "city": "绵阳市", "type": "办公用品", "sub_type": "笔" },
-  { "number": 2457, "province": "四川省", "city": "南充市", "type": "办公用品", "sub_type": "笔" },
-  { "number": 2458, "province": "四川省", "city": "乐山市", "type": "办公用品", "sub_type": "笔" },
-  { "number": 4004, "province": "四川省", "city": "成都市", "type": "办公用品", "sub_type": "纸张" },
-  { "number": 3077, "province": "四川省", "city": "绵阳市", "type": "办公用品", "sub_type": "纸张" },
-  { "number": 3551, "province": "四川省", "city": "南充市", "type": "办公用品", "sub_type": "纸张" },
-  { "number": 352, "province": "四川省", "city": "乐山市", "type": "办公用品", "sub_type": "纸张" }
-]
-}}`
-      }
+      },
+      themeCfg: {},
     })
   }
 ]
--- plugins/antv/antv-pivot-sheet.vue
+++ plugins/antv/antv-pivot-sheet.vue
@@ -6,35 +6,36 @@
 import { watch, onMounted, ref, onUnmounted, onBeforeUnmount } from 'vue'
 import { useResizeObserver } from '@vueuse/core'
 
-const { S2Event, PivotSheet, TableSheet } = S2
+const { S2Event, PivotSheet, TableSheet, getPalette, generatePalette } = S2
 
-const props = defineProps(['dataCfg', 'options'])
+const props = defineProps(['dataCfg', 'options', 'themeCfg', 'brandColor'])
 
 const elRef = ref()
 
 let s2
 
-onMounted(async () => {
-  s2 = new PivotSheet(elRef.value, props.dataCfg, props.options)
-  s2.render()
+onMounted(() => update())
+onBeforeUnmount(() => s2.destroyed())
 
-  useResizeObserver(elRef, ([e]) => {
-    s2.changeSheetSize(e.borderBoxSize[0].inlineSize, e.borderBoxSize[0].blockSize)
-    s2.render(false)
-  })
-
-  onBeforeUnmount(() => {
-    s2.destroyed()
-  })
+useResizeObserver(elRef, ([e]) => {
+  s2.changeSheetSize(e.borderBoxSize[0].inlineSize, e.borderBoxSize[0].blockSize)
+  s2.render(false)
 })
 
-watch(() => JSON.parse(JSON.stringify(props)), e => {
+watch(() => JSON.parse(JSON.stringify(props)), update)
+
+function update() {
+  const { dataCfg, options, themeCfg, brandColor } = props
   s2?.destroy()
-  s2 = new PivotSheet(elRef.value, e.dataCfg, {
-    width: s2.options.width,
-    height: s2.options.height,
-    ...e.options,
+  s2 = new PivotSheet(elRef.value, dataCfg, {
+    width: s2?.options.width,
+    height: s2?.options.height,
+    ...options,
+  })
+  ;(themeCfg || brandColor) && s2.setThemeCfg({
+    palette: brandColor ? generatePalette({ ...getPalette(themeCfg.name), brandColor }) : void 0,
+    ...themeCfg,
   })
   s2.render()
-})
+}
 </script>
